704278920880ecba35ea7aaca1bdef313be79370
davidhernandezz <145987800+davidhernandezz@users.noreply.github.com>
2023-09-25 17:09:18 +0200

    Update convective_analytic_method_2.m

--- convective_analytic_method_2.m
+++ convective_analytic_method_2.m
@@ -10,18 +10,18 @@ syms x y u v
 u(x, y) = cos(2*pi*x) * sin(2*pi*y);
 v(x, y) = - sin(2*pi*x) * cos(2*pi*y);
 
-convective_u = u * diff(u, x) + u * diff(u, y);
-convective_v = v * diff(v, x) + v * diff(v, y);
+convective_u = u * diff(u, x) + v * diff(u, y);
+convective_v = u * diff(v, x) + v * diff(v, y);
 
 f_u = matlabFunction(convective_u, 'Vars', [x y]);
 f_v = matlabFunction(convective_v, 'Vars', [x y]);
 for i = 1:N
     for j = 1:N
-        convective_analytic_u(j, i) = f_u(xu(1, i), yu(1, j));
-        convective_analytic_v(j, i) = f_v(xv(1, i), yv(1, j));
+        convective_analytic_u_aux(i, j) = f_u(xu(1, i), yu(1, j));
+        convective_analytic_v_aux(i, j) = f_v(xv(1, i), yv(1, j));
     end
 end
-% 
-% convective_analytic_u = print_field(convective_analytic_u_aux);
-% convective_analytic_v = print_field(convective_analytic_v_aux);
+ 
+convective_analytic_u = print_field(convective_analytic_u_aux);
+convective_analytic_v = print_field(convective_analytic_v_aux);
 
