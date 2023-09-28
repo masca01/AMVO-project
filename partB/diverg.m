f4f01e723581810d7a6cbd57f61a6aae4a9c8c18
icspet <alexandre.juanico@estudiantat.upc.edu>
2023-09-28 19:35:35 +0200

    s

--- partB/diverg.m
+++ partB/diverg.m
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function  [d] = diverg(u, v, N, L)  % velocity fields u and v in halo_update
 
 %Inicialitzem
@@ -19,3 +20,26 @@ end
 end
 
 
+=======
+function  [d] = diverg(u, v, N, L)  % velocity fields u and v in halo_update
+
+%Inicialitzem
+d = zeros(N, N);
+delta = L/N;
+
+for i = 2:N+1
+    for j = 2:N+1
+        % integral de la vivergencia du/dx
+        du_dx = ((u(i, j) - u(i, j-1)) / delta)*delta^2;
+        
+        % integral de la divergencia dv/dy
+        dv_dy= ((v(i, j) - v(i-1, j)) / delta)*delta^2;
+
+        % divergence = du/dx + dv/dy 
+        d(i-1, j-1) = du_dx + dv_dy;
+    end
+end
+end
+
+
+>>>>>>> Stashed changes
--- partB/vector2Matrix.m
+++ partB/vector2Matrix.m
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function p_poisson_matrix = vector2Matrix(p, N)
 
 k = 1;
@@ -9,3 +10,16 @@ for i = N:-1:1
 end
 
 p_poisson_matrix = halo_update(p_poisson_matrix_aux);
+=======
+function p_poisson_matrix = vector2Matrix(p, N)
+
+k = 1;
+for i = N:-1:1
+    for j = 1:N
+        p_poisson_matrix_aux(i, j) = p(k, 1);
+        k = k + 1;
+    end
+end
+
+p_poisson_matrix = halo_update(p_poisson_matrix_aux);
+>>>>>>> Stashed changes
