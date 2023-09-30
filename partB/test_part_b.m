11411632bdd996c3c5a09f68440e3a606f7037f2
masca01 <valeri.masca@gmail.com>
2023-09-30 17:47:55 +0200

    Update on gradient

--- partB/test_part_b.m
+++ partB/test_part_b.m
@@ -1,8 +1,7 @@
 % main to test part B
 
-clear all
+clear
 close all
-clc
 
 L = 1;
 N = 7;
@@ -32,14 +31,19 @@ v_halo = halo_update(first_halo(v,N));
 
 % calcul de la divergencia de la velocitat
 d = diverg(u_halo, v_halo, N, L);
+
 % es transforma la matriu de la divergencia en un vector
 b = field2vector(d);
+
 % es defineix la matriu A
 A = A_matrix(N);
+
 % es calcula el terme p a partir de la matriu A i el vector b
 p_poisson_vector = inversa(A, b);
+
 % es transforma el terme p, el qual es un vector, en una matriu de NxN
 p_poisson_matrix = vector2Matrix(p_poisson_vector, N);
+
 % calcul del gradient de la matriu de la p (pas 5) 
 [du_dx, du_dy] = gradient_matrix(p_poisson_matrix, N, L);
 
@@ -50,5 +54,6 @@ v_n1 = v - du_dy;
 % es pasen les matrius de velocitat a halo
 un1_halo = halo_update(first_halo(u_n1,N));
 vn1_halo = halo_update(first_halo(v_n1,N));
+
 % calcul de la divergencia de u^(n+1)   (pas 7) i es comprova que dona 0
-resultx = diverg(un1_halo, vn1_halo, N, L);
+result = diverg(un1_halo, vn1_halo, N, L);
