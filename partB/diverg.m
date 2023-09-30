2b024089d398a9375cdfc4440fa312ad24c47afd
masca01 <valeri.masca@gmail.com>
2023-09-30 17:35:05 +0200

    Update

--- partB/diverg.m
+++ partB/diverg.m
@@ -1,21 +1,20 @@
-function d = diverg(u, v, N, L)  % velocity fields u and v in halo_update
-% divergence = du/dx + dv/dy 
+function  [d] = diverg(u, v, N, L)  % velocity fields u and v in halo_update
+
+%Inicialitzem
+d = zeros(N, N);
 delta = L/N;
+
 for i = 2:N+1
     for j = 2:N+1
-
-        % du/dx
-        du_dx(i-1, j-1) = (u(i, j) - u(i, j-1)) / delta;
+        % integral de la vivergencia du/dx
+        du_dx = ((u(i, j) - u(i, j-1)) / delta) * delta^2;
         
-        % dv/dy
-        dv_dy(i-1, j-1) = (v(i, j) - v(i+1, j)) / delta;
+        % integral de la divergencia dv/dy
+        dv_dy= ((v(i, j) - v(i-1, j)) / delta) * delta^2;
 
-
-        % divergence
-        d(i-1, j-1) = du_dx(i-1, j-1) + dv_dy(i-1, j-1);
-
+        % divergence = du/dx + dv/dy 
+        d(i-1, j-1) = du_dx + dv_dy;
     end
 end
 end
-
 
--- partB/test_part_b.m
+++ partB/test_part_b.m
@@ -5,25 +5,30 @@ close all
 clc
 
 L = 1;
-N = 5;
+N = 7;
 
-% es fiquen valors de u i v (tot 0 i algun punt on no es 0)
-u  =[0 0 0 0 0;
-    0 0 1 0 0;
-    0 2 1 0 0;
-    0 0 3 1 0;
-    0 0 0 0 0];
-v  =[0 0 0 0 0;
-    0 0 3 0 0;
-    0 1 4 0 0;
-    0 0 0 1 0;
-    0 0 0 0 0];
+% es posen valors de u i v (tot 0 i excepte algun punt)
+% aixo ja ho canviarem que queda molt cutre
+u  =[0 0 0 0 0 0 0;
+    0 0 1 0 0 0 0;
+    0 -1 2 0 1 -1 0;
+    0 0 -2 1 0 9 -9;
+    0 -1 2 0 1 -1 0;
+    0 0 -2 1 0 9 -9
+    0 1 0 0 0 0 0];
+
+v  =[0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0;
+    0 0 1 0 0 0 0;
+    0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0;
+    0 0 0 0 0 0 -1];
 
 % halo de les velocitats per a poder tractar tots els nodes be
-u_halo = first_halo(u, N);
-v_halo = first_halo(v, N);
-u_halo = halo_update(u_halo);
-v_halo = halo_update(v_halo);
+
+u_halo = halo_update(first_halo(u,N));
+v_halo = halo_update(first_halo(v,N));
 
 % calcul de la divergencia de la velocitat
 d = diverg(u_halo, v_halo, N, L);
@@ -35,27 +40,15 @@ A = A_matrix(N);
 p_poisson_vector = inversa(A, b);
 % es transforma el terme p, el qual es un vector, en una matriu de NxN
 p_poisson_matrix = vector2Matrix(p_poisson_vector, N);
-
-
-
-% a partir d'aqui es on crec que comença a fallar, que no acabo d'entendre
-% el que demana
-
-
 % calcul del gradient de la matriu de la p (pas 5) 
-[du_dx, du_dy] = gradient_matrix(p_poisson_matrix, L, N);
+[du_dx, du_dy] = gradient_matrix(p_poisson_matrix, N, L);
 
 % calcul de u^(n+1)    (pas 6)
 u_n1 = u - du_dx;
 v_n1 = v - du_dy;
 
 % es pasen les matrius de velocitat a halo
-un1_halo = first_halo(u_n1, N);
-vn1_halo = first_halo(v_n1, N);
-un1_halo = halo_update(un1_halo);
-vn1_halo = halo_update(vn1_halo);
-% calcul de la divergencia de u^(n+1)   (pas 7)
-[resultx, resulty] = diverg(un1_halo, vn1_halo, N, L);
-% hauria de donar 0 pero no dona, jo crec que l'error ha destar en la
-% funció que fa el gradient de la pressio xq no acabo de vuere si es fa un
-% u, en u i v, si despres es sumen, etc
+un1_halo = halo_update(first_halo(u_n1,N));
+vn1_halo = halo_update(first_halo(v_n1,N));
+% calcul de la divergencia de u^(n+1)   (pas 7) i es comprova que dona 0
+resultx = diverg(un1_halo, vn1_halo, N, L);
