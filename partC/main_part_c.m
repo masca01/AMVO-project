348c799ed70f4a75e6707b23ae565a27ff27ab2d
masca01 <valeri.masca@gmail.com>
2023-10-01 12:00:54 +0200

    Pressure field

--- partC/main_part_c.m
+++ partC/main_part_c.m
@@ -6,4 +6,11 @@ L=1;
 % evaluate time step for stability
 [pos_x_u, pos_y_u, pos_x_v, pos_y_v, u, v] = set_velocity_field (N, L);
 time = eval_time_step (N, L, u, v);
-[p] = set_pressure_field (N, L, time);
+[pos_x_p, pos_y_p, p] = set_pressure_field (N, L, time);
+
+X = L/N:L/N:L;
+Y = L:-L/N:L/N;
+figure (1)
+hold on
+surf(X, Y, p)
+title('Pressure field')
--- partC/set_pressure_field.m
+++ partC/set_pressure_field.m
@@ -4,8 +4,9 @@ function [pos_x_p, pos_y_p, p] = set_pressure_field (N, L, time)
 % N --> number of cells
 % L --> domain size
 
+visc = (N / L) / 100;  %viscosity (Reynolds number should be around 100)
 rho = 1.29; %air density [kg/m3]
-visc = 100; %viscosity (Reynolds number should be around 100)
+F = exp(-8 * pi^2 * visc * time);
 
 % vector definition
 pos_x_p = zeros(N, N);
@@ -19,7 +20,7 @@ for i = 1:N
     end
 end
 %pos_y_p
-pos_y_u(N, :) = (L/(N)) / 2;
+pos_y_p(N, :) = (L/(N)) / 2;
 for i = N-1:-1:1
     for j = 1:N
         pos_y_p(i, j) = ((L/(N)) / 2) + (L/N) * (N-i);
@@ -27,7 +28,7 @@ for i = N-1:-1:1
 end
 for i = 1:N
     for j = 1:N
-        p(i, j) = cos(2*pi*pos_x_u(i, j)) * sin(2*pi*pos_y_u(i, j)); %  modify according to velocity field    
+        p(i, j) = - rho * F^2 * ( ( ( cos(2*pi*pos_x_p(i, j) ) )^2 / 2 ) + ( ( cos(2*pi*pos_y_p(i, j)) )^2 / 2 ) ); %  modify according to pressure field    
     end
 end
 
