3eaef51ed5cbe48c68e06a813e113a792db979b9
masca01 <valeri.masca@gmail.com>
2023-10-02 12:28:42 +0200

    Part C running div = 0

--- partC/main_part_c.m
+++ partC/main_part_c.m
@@ -1,9 +1,10 @@
 %% main part C
 clear
+warning('off');
 
 N=7;
 L=1;
-numSteps = 1;
+numSteps = 10;
 s = ( L / N )^2;
 
 syms x y
@@ -17,55 +18,71 @@ v_print = zeros(1, numSteps);
 % evaluate time step for stability
 [pos_x_u, pos_y_u, pos_x_v, pos_y_v, u, v] = set_velocity_field(N, L, u_syms, v_syms, x, y);
 
-[pos_x_p, pos_y_p, p] = set_pressure_field (N, L, 0);
+time = eval_time_step (N, L, u, v);
 
-[dp_dx, dp_dy] = gradient_matrix(halo_update(p), N, L);
-up = u;
-vp = v;
+[conv_u, conv_v, diff_u, diff_v] = partA(N, L, u, v);
 
+Ru = ((-conv_u / s) + (diff_u / s));
+
+Rv = ((-conv_v / s) + (diff_v / s));
+
+up = u + time * (Ru);
+vp = v + time * (Rv);
+
+result_div_up = diverg(halo_update(up), halo_update(vp), N, L); %shouldnt be 0
+
+[result_div, u_n1, v_n1] = partB(N,L,up,vp); %should be 0
+
+%[pos_x_p, pos_y_p, p] = set_pressure_field (N, L, 0);
+
+%[dp_dx, dp_dy] = gradient_matrix(halo_update(p), N, L);
+% up = u;
+% vp = v;
+%
 un = u;
 vn = v;
-
-un_new = up - dp_dx;
-vn_new = vp - dp_dy;
-
-time = zeros(1, numSteps);
-timePrint = zeros(1, numSteps);
-
+%
+% un_new = up - dp_dx;
+% vn_new = vp - dp_dy;
+%
+%time = zeros(1, numSteps);
+% timePrint = zeros(1, numSteps);
+%
 for i = 1:numSteps
 
-    un_old = un;
-    vn_old = vn;
+    % u_n = u_n1;
+    % v_n = v_n1;
 
-    un = un_new; %new becomes old
-    vn = vn_new; %new becomes old
+    time(i) = eval_time_step (N, L, u_n1, v_n1);
 
-    time(i) = eval_time_step (N, L, un, vn);
-
-    [conv_u, conv_v, diff_u, diff_v] = partA(N, L, un_old, vn_old);
+    [conv_u, conv_v, diff_u, diff_v] = partA(N, L, un, vn);
 
     Rold_u = ((-conv_u / s) + (diff_u / s)); %old R
 
     Rold_v = ((-conv_v / s) + (diff_v / s)); %old R
 
-    [conv_u, conv_v, diff_u, diff_v] = partA(N, L, un, vn);
+    [conv_u, conv_v, diff_u, diff_v] = partA(N, L, u_n1, v_n1);
 
     Rnew_u = ((-conv_u / s) + (diff_u / s)); %new R
 
     Rnew_v = ((-conv_v / s) + (diff_v / s)); %new R
 
-    up = un + time(i) * (1.5 * Rnew_u - 0.5 * Rold_u);
-    vp = vn + time(i) * (1.5 * Rnew_v - 0.5 * Rold_v);
+    up = u_n1 + time(i) * (1.5 * Rnew_u - 0.5 * Rold_u);
+    vp = v_n1 + time(i) * (1.5 * Rnew_v - 0.5 * Rold_v);
 
-    [pos_x_p, pos_y_p, p] = set_pressure_field (N, L, time(i));
-    [dp_dx, dp_dy] = gradient_matrix(halo_update(p), N, L);
+    % [pos_x_p, pos_y_p, p] = set_pressure_field (N, L, time(i));
+    % [dp_dx, dp_dy] = gradient_matrix(halo_update(p), N, L);
 
-    un_new = up - dp_dx; %new velocity u_n+1
-    vn_new = vp - dp_dy;
+    result_div_up = diverg(halo_update(up), halo_update(vp), N, L); %shouldnt be 0
 
-    u_print(i) = un_new(3,3);
-    v_print(i) = vn_new(3,3);
-    timePrint(i+1) = timePrint(i) + time(i);
+    un = u_n1;
+    vn = v_n1;
+
+    [result_div, u_n1, v_n1] = partB(N,L,up,vp); %should be 0
+
+    % u_print(i) = u_n1(3,3);
+    % v_print(i) = v_n1(3,3);
+    % timePrint(i+1) = timePrint(i) + time(i);
 end
 
 
---
+++ partC/partB.m
@@ -0,0 +1,31 @@
+function [result_div, u_n1, v_n1] = partB(N,L,u,v)
+
+
+% halo de les velocitats per a poder tractar tots els nodes be
+
+u_halo = halo_update(u);
+v_halo = halo_update(v);
+
+% calcul de la divergencia de la velocitat
+d = diverg(u_halo, v_halo, N, L);
+% es transforma la matriu de la divergencia en un vector
+b = field2vector(d);
+% es defineix la matriu A
+A = A_matrix(N);
+% es calcula el terme p a partir de la matriu A i el vector b
+p_poisson_vector = inversa(A, b);
+% es transforma el terme p, el qual es un vector, en una matriu de NxN
+p_poisson_matrix = vector2Matrix(p_poisson_vector, N);
+% calcul del gradient de la matriu de la p (pas 5) 
+[du_dx, du_dy] = gradient_matrix(p_poisson_matrix, N, L);
+
+% calcul de u^(n+1)    (pas 6)
+u_n1 = u - du_dx;
+v_n1 = v - du_dy;
+
+% es pasen les matrius de velocitat a halo
+un1_halo = halo_update(u_n1);
+vn1_halo = halo_update(v_n1);
+% calcul de la divergencia de u^(n+1)   (pas 7) i es comprova que dona 0
+result_div = diverg(un1_halo, vn1_halo, N, L);
+
