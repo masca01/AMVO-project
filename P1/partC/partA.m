ad00780d5a20ee1073b40861b22d0fa6865c2f93
masca01 <valeri.masca@gmail.com>
2023-10-09 11:48:33 +0200

    Update

--- P1/partC/partA.m
+++ P1/partC/partA.m
@@ -2,7 +2,7 @@ function [conv_u, conv_v, diff_u, diff_v] = partA(N, L, u, v)
 
     %% CONVECTIVE
 
-    [conv_u, conv_v] = convective_term_comp(halo_update(u), halo_update(v), N, L);
+    [conv_u, conv_v] = convective_term(halo_update(u), halo_update(v), N, L);
 
     %% DIFFUSIVE
 
